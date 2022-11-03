2a60f90bd5d80572da1168eabffd39a675e3805c
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-11-03 21:02:37 +0800

    修改App.jsx-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      name:"tom"
+    }
+  }
+  handdleClick=()=>{
+    this.setState({
+      name:"jarry"
+    })
+  }
   render() {
+    let {name}=this.state;
     return (
       <div>
          <h3>App组件</h3>
+         <p>{name}</p>
+         <button onClick={this.handdleClick}>change name</button>
       </div>
     )
   }
